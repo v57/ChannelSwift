d1135691e75bb77deb1ff42a7e46629809c07fa9
Dmitry <d@kozlov.ru>
2025-04-28 01:13:26 +0700

    Added iOS, visionOS, tvOS, watchOS, macCatalyst support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let name = "ChannelSwift"
 let package = Package(
   name: name,
-  platforms: [.macOS(.v10_15)],
+  platforms: [.macOS(.v10_15), .iOS(.v13), .visionOS(.v1), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
   products: [.library(name: name, targets: [name])],
   targets: [.target(name: name), .testTarget(name: "ChannelSwiftTests", dependencies: ["ChannelSwift"])]
 )
