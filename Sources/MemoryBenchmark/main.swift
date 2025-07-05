50f3fa79430bdf06c89a34f345f361a346815362
Dmitry <d@kozlov.ru>
2025-07-06 01:22:46 +0300

    Memory benchmark: Added sync api and break test

--- Sources/MemoryBenchmark/main.swift
+++ Sources/MemoryBenchmark/main.swift
@@ -10,11 +10,28 @@ import Channel
 let channel = Channel<Void>()
 let client = channel.connect(2049)
 
+@MainActor
+func runSync() async throws {
+  for try await _: Int in client.values("stream/sync") { }
+}
+@MainActor
+func runSyncWithBreak() async throws {
+  for try await value: Int in client.values("stream/sync") {
+    if value == 2 { break }
+  }
+}
+
 var sent = 0
 while true {
-  for try await _: Int in client.values("stream/values") {
-    
+  try await runSync()
+  sent += 1
+  if sent / 1000 != (sent - 1) / 1000 {
+    print("Sent", sent, client.ws.pendingCount)
   }
+}
+sent = 0
+while true {
+  try await runSyncWithBreak()
   sent += 1
   if sent / 1000 != (sent - 1) / 1000 {
     print("Sent", sent, client.ws.pendingCount)
